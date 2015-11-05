5368f3befb8928085ac322c10bbdd70ac2488744
karvel <jadeilyn@yahoo.com>
2015-11-04 22:57:47 -0800

    Beginning functions in JavaScript. Initial commit.

---
+++ app02.js
@@ -0,0 +1,13 @@
+/*
+Elanna Grossman
+November 4, 2015
+GeekWise Javascript I
+*/
+
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+
+console.log("Console output: " + firstName);
+alert(lastName);
+var confirmVar = confirm("These popups are annoying!");
+alert("Results of confirm: " + confirmVar);
